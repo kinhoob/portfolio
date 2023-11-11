9fa5fee55a21431a39b2545a25d77aa03ec8f650
Marco Davi <marcodavibdemelo@gmail.com>
2023-11-11 13:49:52 -0300

    //

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,41 @@ let swiperProjects = new Swiper(".projects__container", {
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+        contactMessage.classList.remove('color-blue');
+        contactMessage.classList.add('color-red');
+        contactMessage.textContent = 'Escreva em todos os campos';
+    } else {
+        emailjs.sendForm('service_v8p4w6l', 'template_1c69no7', 'contact-form', 'laoQ4ko84LKm4dx1d')
+            .then(() => {
+                contactMessage.classList.remove('color-red');
+                contactMessage.classList.add('color-blue');
+                contactMessage.textContent = 'Mensagem Enviada';
+
+                setTimeout(() => {
+                    contactMessage.textContent = '';
+                }, 5000);
+            })
+            .catch((error) => {
+                alert('Ops! Alguma coisa está errada. ' + error);
+            });
+
+        contactName.value = '';
+        contactEmail.value = '';
+        contactProject.value = '';
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
